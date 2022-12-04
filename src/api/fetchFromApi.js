5590dc0617fadb224df1b5b4cf93dcf051fbabdb
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-12-04 20:15:38 +0100

    bugfix

--- src/api/fetchFromApi.js
+++ src/api/fetchFromApi.js
@@ -35,7 +35,7 @@ export async function fetchMovieCastById(movieId) {
 
 export async function fetchMovieByQuery(query) {
   const fetchedData = await fetch(
-    `${BASE_URL}search/movie/?api_key=${API_KEY}&query=${query}`
+    `https://api.themoviedb.org/3/search/movie/?api_key=${API_KEY}&query=${query}`
   );
   const response = await fetchedData.json();
   return response;
